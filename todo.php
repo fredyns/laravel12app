d0bb920df443bca5080a967f5329751cb9054ba4
Fredy Nurman Saleh <email@fredyns.net>
2025-03-27 06:59:14 +0700

    already exist

--- todo.php
+++ todo.php
@@ -2,9 +2,6 @@
 /**
  * TODO:
  * 8. ganti tabel ID dengan UUID
- * 9. cari tau, install, tabel password_reset_token
- * 10. cari tau, install, tabel failed_jobs
- * 12. cari tau, install, tabel sessions
  * 14. copy deploy script
  * 15. copy dev files
  * 16. setup docker
