b0829a7dce404431479021814d7d2bd5f5146c93
Fredy Nurman Saleh <email@fredyns.net>
2025-03-26 22:50:25 +0700

    done

--- todo.php
+++ todo.php
@@ -1,19 +1,11 @@
 <?php
 /**
  * TODO:
- * 1. belajar sekilas jetstream
- * 2. belajar sekilas sanctum
- * 3. install sanctum dgn benar
- * 4. cek migrasi & aset sanctum
- * 5. pastikan bisa login
  * 6. install spatie/permission
- * 7. cek migrasi & export file dr sanctum
  * 8. ganti tabel ID dengan UUID
  * 9. cari tau, install, tabel password_reset_token
  * 10. cari tau, install, tabel failed_jobs
- * 11. cari tau, install, tabel personal_access_token
  * 12. cari tau, install, tabel sessions
- * 13. set storage
  * 14. copy deploy script
  * 15. copy dev files
  * 16. setup docker
