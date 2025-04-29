fcaa1a43dbb00ae4f2a5d8c74c3b40d083d42167
Fredy Nurman Saleh <email@fredyns.net>
2025-04-30 06:51:49 +0700

    set next

--- todo.php
+++ todo.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * TODO:
- * 17.
+ * 17.ANY api . api.status › Api\AuthController@status
+ * 18.POST api/registration . api.registration › Api\AuthController@registration
+ * 19.POST api/login . api.login › Api\AuthController@login
+ * 20.POST api/logout . api.logout › Api\AuthController@logout
+ * 21.cek GET|HEAD api/user  api.user
+ * 22.
  *
  */
