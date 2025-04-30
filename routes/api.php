1a180dcec27765a9e4f8bde36001ce8650d649b0
Fredy Nurman Saleh <email@fredyns.net>
2025-04-30 07:12:37 +0700

    api auth

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// status
+Route::any('/', [AuthController::class, 'status'])->name('api.status');
+
+// login
+Route::post('login', [AuthController::class, 'login'])->name('api.login');
+
+// registration
+Route::post('registration', [AuthController::class, 'registration'])->name(
+    'api.registration'
+);
+
+Route::name('api.')
+    ->middleware('auth:sanctum')
+    ->group(function () {
+        // current user
+        Route::get('user', fn(Request $req) => ['data' => $req->user()])->name('user');
+
+        // logout
+        Route::post('logout', [AuthController::class, 'logout'])->name(
+            'logout'
+        );
+    });
