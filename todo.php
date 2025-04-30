a8b1b366608b75601f4de6f9d670a782fd692dad
Fredy Nurman Saleh <email@fredyns.net>
2025-04-30 07:16:03 +0700

    next

--- todo.php
+++ todo.php
@@ -1,6 +1,26 @@
 <?php
 /**
  * TODO:
- * 22.
+ *  22.a.generate migration table
+ *  22.b.generate migration fk
+ *  22.c.generate migration permission
+ *  23.generate factory
+ *  24.generate seeder
+ *  25.generate models
+ *  26.generate routes
+ *  27.generate policy
+ *  28.generate controller
+ *  29.generate request
+ *  30.generate views
+ *  31.generate lang
+ *  32.generate livewire
+ *  33.generate livewire  views
+ *  34.generate livewire lang
+ *  35.generate api
+ *  36.generate resources
+ *  37.
+ *  38.
+ *  39.
+ *  4.
  *
  */
